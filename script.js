160c382c149bbb04272a85b664a8aa8b5e520c4f
chrisPicano <cpicano25@gmail.com>
2021-01-13 21:25:16 -0500

    Fixed Character Bug

--- script.js
+++ script.js
@@ -1,66 +1,85 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var length = 0;
+var passwordText = document.querySelector("#password");
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  console.log(password)
   
 
   passwordText.value = password;
 
 }
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//generateBtn.addEventListener("click", writePassword);
+
 
 function generatePassword() {
-  var newPassword = "";
+  var password = ""
+  
   var length = prompt("How many characters would you like your password to be?");
 
-console.log(length)
 
-if ( length < 8){
-  alert("Sorry must be longer than 8 characters :(");
-  return 
-}
-if ( length > 128){
-  alert("Sorry must be less than 128 characters!")
-  return
-}
 
-var lowercase = confirm("Do you want lower case?");
+  if ( length < 8){
+    alert("Sorry must be longer than 8 characters :(");
+    return 
+  }
+  if ( length > 128){
+    alert("Sorry must be less than 128 characters!")
+    return
+  }
+
+  var lowercase = confirm("Do you want lower case?");
+  var uppercase = confirm("Do you want an upper case?");
+  var symbol = confirm("Do you want a special character?");
+  var number = confirm("Do you want a number?");
+
   if (lowercase === true){
-    newPassword += getRandomLower()
+    console.log("PASS1", password)
+    password += getRandomLower()
   }
 
-var uppercase = confirm("Do you want an upper case?");
+
   if (uppercase === true){
-    newPassword += getRandomUpper()
+    console.log("PASS2", password)
+    password += getRandomUpper()
   }
-var symbol = confirm("Do you want a special character?");
+
   if (symbol === true) {
-    newPassword += getRandomSymbol()
+    console.log("PASS3", password)
+    password += getRandomSymbol()
   }
-var number = confirm("Do you want a number?");
+
   if (number === true){
-    newPassword += getRandomNumber()
+    console.log("PASS4", password)
+    password += getRandomNumber()
+  }
+  var iterations = Number(length) - password.length
+  console.log("PASS", password, Number(length), password.length, iterations)
+  for (var i = 0; i < iterations; i++) {
+      console.log("Here")
+      password += "1";
   }
 
 console.log(lowercase)
 console.log(uppercase)
 console.log(symbol)
 console.log(number)
-return newPassword
+  return password
+
 }
 
 
 
-for (i = 0; i < length; i++) {
 
 //function for random lower case letter
 function getRandomLower() {
   return String.fromCharCode(Math.floor(Math.random() * 26) +97);
 }
+
 //function for random upper case latter (numbers are represented on browser character chart)
 function getRandomUpper() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
@@ -76,7 +95,8 @@ function getRandomSymbol() {
 
 
 
-}
+
+
 //duplicated functions for outside loop
 function getRandomLower() {
   return String.fromCharCode(Math.floor(Math.random() * 26) +97);
@@ -85,6 +105,8 @@ function getRandomLower() {
 function getRandomUpper() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
+
+
 function getRandomNumber() {
   return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
@@ -93,3 +115,5 @@ function getRandomSymbol() {
   var index = Math.floor(Math.random() * symbols.length)
   return symbols.charAt(index)
 }
+
+generateBtn.addEventListener("click", writePassword);
